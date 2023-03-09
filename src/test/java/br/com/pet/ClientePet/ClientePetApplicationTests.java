be4524d60c0cef9dae46c4406bfa77aeee41525b
Junji Ogaki <rosivelto_gato@yahoo.com.br>
2023-03-08 23:59:44 -0300

    commit inicial

---
+++ src/test/java/br/com/pet/ClientePet/ClientePetApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.pet.ClientePet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientePetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
